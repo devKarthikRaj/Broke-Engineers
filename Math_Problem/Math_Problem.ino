0cdcf026806a9006458b5ed0b921f65cd49c54b5
garage555 <119643739+garage555@users.noreply.github.com>
2022-12-02 16:12:19 +0800

    Added Changes
    Change the questions and now prints 10 questions

--- Math_Problem/Math_Problem.ino
+++ Math_Problem/Math_Problem.ino
@@ -1,3 +1,4 @@
+//Keypad_Reader file
 #include <Keypad.h>
 
 #define ROW_NUM     4 // four rows
@@ -15,9 +16,11 @@ byte pin_column[COLUMN_NUM] = {16, 4, 0, 2};   // GIOP16, GIOP4, GIOP0, GIOP2
 
 Keypad customKeypad = Keypad( makeKeymap(keys), pin_rows, pin_column, ROW_NUM, COLUMN_NUM );
 
+
+//Variables for math_problem()
 int i = 0;
 String ans = "0";
-String qn;
+String qn = "0";
 
 void setup() {
   Serial.begin(115200);
@@ -33,7 +36,7 @@ void loop() {
 String math_problem(){
 
   //Randomly choose questions
-  i = random(1,10);
+  i = random(1,9);
 
   if(i == 1){
     qn = "1 + 3";
@@ -46,49 +49,42 @@ String math_problem(){
   }
 
   else if(i == 3){
-    qn = "3";
+    qn = "30 / 10";
     ans = "3";
   }
 
   else if(i == 4){
-    qn = "4";
-    ans = "4";
+    qn = "74 - 73";
+    ans = "1";
   }
 
   else if(i == 5){
-    qn = "5";
+    qn = "2 + 3";
     ans = "5";
   }
 
   else if(i == 6){
-    qn = "6";
+    qn = "2 * 3";
     ans = "6";
   }
  
   else if(i == 7){
-    //Serial.setCursor(2,0);
     qn = "56 / 7";
-    
     ans = "8";
   }
 
   else if(i == 8){
-    qn = "8";
-    ans = "8";
-  }
-
-  else if(i == 9){
-    qn = "9";
-    ans = "9";
-  }
-
-  else if(i == 10){
     qn = "21 / 3";
-    
     ans = "7";
   }
 
-  Serial.println(qn);
+  else if(i == 9){
+    qn = "78 / 39";
+    ans = "2";
+  }
 
+  //Print the Question
+  Serial.println(qn);
+  //Return answer to Question
   return ans;
 }
