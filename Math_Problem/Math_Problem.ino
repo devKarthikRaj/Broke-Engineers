a15f4b292e7e5b54b2a0d23603ebc36108fec88f
garage555 <119643739+garage555@users.noreply.github.com>
2022-12-02 16:16:21 +0800

    Added Changes
    Removed Keypad_Read

--- Math_Problem/Math_Problem.ino
+++ Math_Problem/Math_Problem.ino
@@ -1,22 +1,3 @@
-//Keypad_Reader file
-#include <Keypad.h>
-
-#define ROW_NUM     4 // four rows
-#define COLUMN_NUM  4 // four columns
-
-char keys[ROW_NUM][COLUMN_NUM] = {
-  {'1', '2', '3', 'A'},
-  {'4', '5', '6', 'B'},
-  {'7', '8', '9', 'C'},
-  {'*', '0', '#', 'D'}
-};
-
-byte pin_rows[ROW_NUM]      = {19, 18, 5, 17}; // GIOP19, GIOP18, GIOP5, GIOP17 connect to the row pins
-byte pin_column[COLUMN_NUM] = {16, 4, 0, 2};   // GIOP16, GIOP4, GIOP0, GIOP2
-
-Keypad customKeypad = Keypad( makeKeymap(keys), pin_rows, pin_column, ROW_NUM, COLUMN_NUM );
-
-
 //Variables for math_problem()
 int i = 0;
 String ans = "0";
